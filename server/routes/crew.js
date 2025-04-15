b42cba27d57e42f4feb6f1b7fa60deae79ebeccd
Hemant <emmeehemant159@gmail.com>
2025-04-16 02:48:08 +0530

    added the backend loginc for sending messiging to and from admin and client

--- server/routes/crew.js
+++ server/routes/crew.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { assignCrew, completeTrip, crewDetail, listCrew, swapShift, transferBus } from "../controllers/crewController.js";
+import { getCrewMessages, respondToMessage } from "../controllers/admin/message.js";
 
 const app = express.Router();
 
@@ -9,5 +10,7 @@ app.get('/crewDetail', crewDetail);
 app.post("/complete_trip", completeTrip);
 app.post("/transfer_bus", transferBus);
 app.post('/swap_shift', swapShift);
+app.put('/message/:id/respond', respondToMessage);
+app.get('/messages/:crewId', getCrewMessages);
 
 export default app;
