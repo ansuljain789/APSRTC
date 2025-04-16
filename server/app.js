d85f60edcb800f65dc50024628181039b47fc4ea
Jain16Akshat <148090508+Jain16Akshat@users.noreply.github.com>
2025-04-17 01:18:16 +0530

    admin page

--- server/app.js
+++ server/app.js
@@ -15,7 +15,6 @@ import routesRoutes from './routes/routes.js';
 import { Bus } from './models/bus.js';
 import adminRoute from './routes/admin.js'
 import { Crew } from './models/crew.js';
-import { Bus } from './models/bus.js';
 
 
 dotenv.config();
@@ -38,7 +37,7 @@ app.use(
 );
 
 // 5. Setup API Routes
-app.use('/api/')
+app.use('/api/');
 app.use('/api/crew', crewDetail);
 app.use('/api/buses', busesRoutes);
 app.use('/api/routes', routesRoutes);
