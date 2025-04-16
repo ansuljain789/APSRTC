f1c82717215fa753133c299261bd8a51d4513834
Subhajit-sahu <subhajitsahu133@gmail.com>
2025-04-16 16:54:59 +0530

    logion and footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom'
-import './App.css'
-import HomePage from './Pages/HomePage'
-import Login from './Main Pages/Authentication/Login'
-import Signup from './Main Pages/Authentication/Signup'
-import ContactUs from './Pages/ContactUs'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import HomePage from './Pages/HomePage';
+import Login from './Main Pages/Authentication/Login';
+import Signup from './Main Pages/Authentication/Signup';
+import ContactUs from './Pages/ContactUs';
+import Footer from './Pages/Footer'; // Make sure the path matches where you save Footer.jsx
 
 function App() {
-
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path='/' element={ <HomePage/>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/contact" element={<ContactUs/>} />
+          <Route path="/contact" element={<ContactUs />} />
         </Routes>
-      </BrowserRouter>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
